49305812fb8ffb2a7dca5f5dbd4c436c350f90fe
NOORADLINA BINTE AFFANDI 2205102C <2205102C@student.tp.edu.sg>
2023-12-10 15:11:28 +0800

    identity

--- Table4URest/Server/Models/ApplicationUser.cs
+++ Table4URest/Server/Models/ApplicationUser.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Threading.Channels;
+using Table4URest.Client;
 
 namespace Table4URest.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+     
     }
 }
--- Table4URest/Server/Program.cs
+++ Table4URest/Server/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.EntityFrameworkCore;
 using Table4URest.Server.Data;
 using Table4URest.Server.Models;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
