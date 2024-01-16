5d3d61962ba267151c8d2ec426d9a22bc3328cfc
NOORADLINA BINTE AFFANDI 2205102C <2205102C@student.tp.edu.sg>
2024-01-16 12:17:59 +0800

    Customer, Serve, Price, Location view update

---
+++ Table4URest/Client/Static/Endpoints.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Table4URest.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string CustomersEndpoint = $"{Prefix}/customers";
+        public static readonly string LocationFiltersEndpoint = $"{Prefix}/locationfilters";
+        public static readonly string PriceFiltersEndpoint = $"{Prefix}/pricefilters";
+        public static readonly string ReservationsEndpoint = $"{Prefix}/reservations";
+        public static readonly string RestaurantsEndpoint = $"{Prefix}/restaurants";
+        public static readonly string ReviewsEndpoint = $"{Prefix}/reviews";
+        public static readonly string ServeFiltersEndpoint = $"{Prefix}/servefilters";
+        public static readonly string StaffsEndpoint = $"{Prefix}/staffs";
+    }
+}
